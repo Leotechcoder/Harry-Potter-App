d19aa156ec0ba16badfccd14be598899888f87c8
Leoto <blabla.13leo@gmail.com>
2024-10-24 20:54:20 -0300

    primer commit

---
+++ src/pages/Personajes.jsx
@@ -0,0 +1,20 @@
+import CrudForm from "../components/CrudForm"
+import Personprovideredit from "../components/Personprovideredit"
+import Table from "../components/Table"
+
+
+const Personajes = () => {
+
+
+  return (
+    <Personprovideredit>
+
+    <CrudForm/>
+
+    <Table/>
+    
+    </Personprovideredit>
+  )
+}
+
+export default Personajes
